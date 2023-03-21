714daccad7805d1f78395266368194f67dbab15f
Angélica Diniz <angelicadiniz_ad@hotmail.com>
2023-03-21 19:10:57 -0300

    first commit

---
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/exercicios/01.js
@@ -0,0 +1,49 @@
+/* 
+! Elabore alguns códigos e imprima o resultado no console usando o console.log, 
+! um para cada operação aritmética básica. 
+! Seu código deve ter duas variáveis, a e b, 
+! definidas no começo com os valores que serão operados. 
+! Escreva códigos para:
+
+*/
+
+/* 
+? 2 - Utilize if/else para escrever um código que retorne o maior de dois números. 
+?     Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
+*/
+
+/* 
+? 3 - Utilize if/else para escrever um código que retorne o maior de três números. 
+?     Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+*/
+
+
+/* 
+? 4 - Utilize if/else para escrever um código que, 
+?     dado um valor recebido como parâmetro, retorne: “positive”, 
+?     se esse valor for positivo; “negative”, 
+?     se esse valor for negativo, e “zero”, 
+?     caso esse valor não seja nem positivo nem negativo.
+*/
+
+
+/* 
+? 5 - Utilize if/else para escrever um código que defina três variáveis 
+?     com os valores dos três ângulos internos de um triângulo. 
+?     Retorne true se os ângulos representarem os ângulos de um triângulo e 
+?     false, caso contrário. 
+?     Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+*/
+
+
+/* 
+? 6 - Utilize switch/case para escrever um código que receba 
+?     o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer.
+*/
+
+
+/* 
+? 7 - Utilize if/else para escrever um código que 
+?     converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. 
+?     Siga as seguintes regras:
+*/
